3ba434365c123e58eeafa08879739742b000e482
774ajaysingh <774ajaysingh@gmail.com>
2026-05-29 17:41:49 +0530

    Update q3.c

---
+++ day_1.c/q3.c
@@ -0,0 +1,19 @@
+//Write a program to Find factorial of a number
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    long long factorial = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for(int i = 1; i <= num; i++) {
+        factorial = factorial * i;
+    }
+
+    printf("Factorial of %d = %lld", num, factorial);
+
+    return 0;
+}
