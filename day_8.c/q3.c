e460eb8ff76ef0d571e02d7c7553d5bdf2744b07
774ajaysingh <774ajaysingh@gmail.com>
2026-06-11 22:27:10 +0530

    .

---
+++ day_8.c/q3.c
@@ -0,0 +1,24 @@
+/*Write a program to Print character triangle. 
+A 
+AB 
+ABC 
+ABCD 
+ABCDE */
+
+#include <stdio.h>
+int main (){
+    int n,i,j;
+    printf("enter the number of rows");
+    scanf("%d",&n);   
+
+    for(i = 1; i <= n; i++) {
+        for(j = 0; j <= i; j++) {
+            printf(" %c", 'A'+ j);
+
+        }
+        printf("/n");
+    }
+
+    return 0;
+
+}
