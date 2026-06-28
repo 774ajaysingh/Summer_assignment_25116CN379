516d000ef8dfe262a945a6a1b64af7ef6b02571d
774ajaysingh <774ajaysingh@gmail.com>
2026-06-28 13:43:21 +0530

    day26

---
+++ day__26/q101.c
@@ -0,0 +1,29 @@
+/*Write a program to Create number guessing 
+game*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int secretNumber, guess;
+    srand(time(0));
+    secretNumber = rand() % 100 + 1;
+    printf("Welcome to the Number Guessing Game!\n");
+    printf("I have selected a number between 1 and 100.\n");
+    printf("Try to guess it!\n");
+
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+
+        if (guess > secretNumber) {
+            printf("Too high! Try again.\n");
+        } else if (guess < secretNumber) {
+            printf("Too low! Try again.\n");
+        } else {
+            printf("Congratulations! You guessed the number %d correctly.\n", secretNumber);
+        }
+    } while (guess != secretNumber);
+
+    return 0;
+}
